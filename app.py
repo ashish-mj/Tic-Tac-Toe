2ec205f09cc84f3631ac6c246827becda86928cb
Ashish M J <ashm.jagadeesh@gmail.com>
2020-12-27 13:55:04 +0530

    Handle Mouse Click

--- app.py
+++ app.py
@@ -14,38 +14,61 @@ window = pygame.display.set_mode((550,550))
 pygame.display.set_caption('Tic-Tac-Toe')
 
 white = (255,255,255)
+red = (255,0,0)
 
-first = pygame.Rect(25,25,150,150)
-second = pygame.Rect(200,25,150,150)
-third = pygame.Rect(375,25,150,150)
+first = pygame.draw.rect(window, white, (25,25,150,150))
+second = pygame.draw.rect(window, white,(200,25,150,150))
+third = pygame.draw.rect(window, white,(375,25,150,150))
 
-forth = pygame.Rect(25,200,150,150)
-fifth = pygame.Rect(200,200,150,150)
-sixth = pygame.Rect(375,200,150,150)
+forth = pygame.draw.rect(window, white,(25,200,150,150))
+fifth = pygame.draw.rect(window, white,(200,200,150,150))
+sixth = pygame.draw.rect(window, white,(375,200,150,150))
 
-seventh = pygame.Rect(25,375,150,150)
-eight = pygame.Rect(200,375,150,150)
-nine = pygame.Rect(375,375,150,150)
+seventh = pygame.draw.rect(window, white,(25,375,150,150))
+eight = pygame.draw.rect(window, white,(200,375,150,150))
+nine = pygame.draw.rect(window, white,(375,375,150,150))
+
 
 while True:
+    
+    pygame.time.delay(100)
     
     for event in pygame.event.get():
         
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+            
+        if event.type == pygame.MOUSEBUTTONUP:
+            position = pygame.mouse.get_pos()
+            
+            if first.collidepoint(position):
+                pygame.draw.rect(window,red,(50,50,100,100))
+                
+            if second.collidepoint(position):
+                pygame.draw.rect(window,red,(225,50,100,100))
+                
+            if third.collidepoint(position):
+                pygame.draw.rect(window,red,(400,50,100,100))
+                
+            if forth.collidepoint(position):
+                pygame.draw.rect(window,red,(50,225,100,100))
+                
+            if fifth.collidepoint(position):
+                pygame.draw.rect(window,red,(225,225,100,100))
+                
+            if sixth.collidepoint(position):
+                pygame.draw.rect(window,red,(400,225,100,100))
+                
+            if seventh.collidepoint(position):
+                pygame.draw.rect(window,red,(50,400,100,100))
+                
+            if eight.collidepoint(position):
+                pygame.draw.rect(window,red,(225,400,100,100))
+                
+            if nine.collidepoint(position):
+                pygame.draw.rect(window,red,(400,400,100,100))
+            
+            
     
-    pygame.draw.rect(window,white,first)
-    pygame.draw.rect(window,white,second)
-    pygame.draw.rect(window,white,third)
-    
-    pygame.draw.rect(window,white,forth)
-    pygame.draw.rect(window,white,fifth)
-    pygame.draw.rect(window,white,sixth)
-    
-    pygame.draw.rect(window,white,seventh)
-    pygame.draw.rect(window,white,eight)
-    pygame.draw.rect(window,white,nine)
-    
-    
-    pygame.display.flip()
+    pygame.display.update()
