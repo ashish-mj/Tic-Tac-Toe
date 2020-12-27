c85580088adf16635c902f5a53a1514df1d97a24
Ashish M J <ashm.jagadeesh@gmail.com>
2020-12-27 11:11:56 +0530

    Handle Close Event

--- app.py
+++ app.py
@@ -6,10 +6,17 @@ Created on Sun Dec 27 10:50:04 2020
 @author: ashish
 """
 
-import pygame
+import pygame,sys
 
 pygame.init()
 
 window = pygame.display.set_mode((550,550))
 pygame.display.set_caption('Tic-Tac-Toe')
 
+while True:
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
