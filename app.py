1fce0542f08e7792eb1d5d86bee0d1e0dabdd6ad
Ashish M J <ashm.jagadeesh@gmail.com>
2020-12-27 11:02:53 +0530

    Window Set-up

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 10:50:04 2020
+
+@author: ashish
+"""
+
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((550,550))
+pygame.display.set_caption('Tic-Tac-Toe')
+
